5db5ee7479a95300bb3c397b623250cc422a22ee
Henry <zhiwei.yang1997@gmail.com>
2019-07-16 17:01:33 -0500

    Saving work

---
+++ UI/MainUI.py
@@ -0,0 +1,305 @@
+import sys
+
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+try:
+    import ttk
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+
+import MainUIBuilder_support
+
+def vp_start_gui():
+    '''Starting point when module is the main routine.'''
+    global val, w, root
+    root = tk.Tk()
+    top = Toplevel1 (root)
+    MainUIBuilder_support.init(root, top)
+    root.mainloop()
+
+w = None
+def create_Toplevel1(root, *args, **kwargs):
+    '''Starting point when module is imported by another program.'''
+    global w, w_win, rt
+    rt = root
+    w = tk.Toplevel (root)
+    top = Toplevel1 (w)
+    MainUIBuilder_support.init(w, top, *args, **kwargs)
+    return (w, top)
+
+def destroy_Toplevel1():
+    global w
+    w.destroy()
+    w = None
+
+class Toplevel1:
+    def __init__(self, top=None):
+        '''This class configures and populates the toplevel window.
+           top is the toplevel containing window.'''
+        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
+        _fgcolor = '#000000'  # X11 color: 'black'
+        _compcolor = '#d9d9d9' # X11 color: 'gray85'
+        _ana1color = '#d9d9d9' # X11 color: 'gray85'
+        _ana2color = '#ececec' # Closest X11 color: 'gray92'
+        self.style = ttk.Style()
+        if sys.platform == "win32":
+            self.style.theme_use('winnative')
+        self.style.configure('.',background=_bgcolor)
+        self.style.configure('.',foreground=_fgcolor)
+        self.style.configure('.',font="TkDefaultFont")
+        self.style.map('.',background=
+            [('selected', _compcolor), ('active',_ana2color)])
+
+        top.geometry("855x616+417+117")
+        top.title("New Toplevel")
+        top.configure(background="#d9d9d9")
+
+        self.predict_btn = tk.Button(top)
+        self.predict_btn.place(relx=0.596, rely=0.86, height=24, width=48)
+        self.predict_btn.configure(activebackground="#ececec")
+        self.predict_btn.configure(activeforeground="#000000")
+        self.predict_btn.configure(background="#d9d9d9")
+        self.predict_btn.configure(disabledforeground="#a3a3a3")
+        self.predict_btn.configure(foreground="#000000")
+        self.predict_btn.configure(highlightbackground="#d9d9d9")
+        self.predict_btn.configure(highlightcolor="black")
+        self.predict_btn.configure(pady="0")
+        self.predict_btn.configure(text='''Predict''')
+
+        self.menubar = tk.Menu(top,font="TkMenuFont",bg=_bgcolor,fg=_fgcolor)
+        top.configure(menu = self.menubar)
+
+        self.TEntry1 = ttk.Entry(top)
+        self.TEntry1.place(relx=0.749, rely=0.13, relheight=0.034
+                , relwidth=0.147)
+        self.TEntry1.configure(takefocus="")
+        self.TEntry1.configure(cursor="ibeam")
+
+        self.Scrolledlistbox1 = ScrolledListBox(top)
+        self.Scrolledlistbox1.place(relx=0.023, rely=0.114, relheight=0.609
+                , relwidth=0.305)
+        self.Scrolledlistbox1.configure(background="white")
+        self.Scrolledlistbox1.configure(disabledforeground="#a3a3a3")
+        self.Scrolledlistbox1.configure(font="TkFixedFont")
+        self.Scrolledlistbox1.configure(foreground="black")
+        self.Scrolledlistbox1.configure(highlightbackground="#d9d9d9")
+        self.Scrolledlistbox1.configure(highlightcolor="#d9d9d9")
+        self.Scrolledlistbox1.configure(selectbackground="#c4c4c4")
+        self.Scrolledlistbox1.configure(selectforeground="black")
+        self.Scrolledlistbox1.configure(width=10)
+
+        self.TLabel1 = ttk.Label(top)
+        self.TLabel1.place(relx=0.094, rely=0.065, height=19, width=62)
+        self.TLabel1.configure(background="#d9d9d9")
+        self.TLabel1.configure(foreground="#000000")
+        self.TLabel1.configure(font="TkDefaultFont")
+        self.TLabel1.configure(relief="flat")
+        self.TLabel1.configure(text='''Course List''')
+
+        self.TLabel2 = ttk.Label(top)
+        self.TLabel2.place(relx=0.503, rely=0.065, height=19, width=107)
+        self.TLabel2.configure(background="#d9d9d9")
+        self.TLabel2.configure(foreground="#000000")
+        self.TLabel2.configure(font="TkDefaultFont")
+        self.TLabel2.configure(relief="flat")
+        self.TLabel2.configure(text='''Prerequisite Classes''')
+
+        self.Scrolledlistbox1_2 = ScrolledListBox(top)
+        self.Scrolledlistbox1_2.place(relx=0.433, rely=0.114, relheight=0.609
+                , relwidth=0.258)
+        self.Scrolledlistbox1_2.configure(background="white")
+        self.Scrolledlistbox1_2.configure(disabledforeground="#a3a3a3")
+        self.Scrolledlistbox1_2.configure(font="TkFixedFont")
+        self.Scrolledlistbox1_2.configure(foreground="black")
+        self.Scrolledlistbox1_2.configure(highlightbackground="#d9d9d9")
+        self.Scrolledlistbox1_2.configure(highlightcolor="#d9d9d9")
+        self.Scrolledlistbox1_2.configure(selectbackground="#c4c4c4")
+        self.Scrolledlistbox1_2.configure(selectforeground="black")
+        self.Scrolledlistbox1_2.configure(width=10)
+
+        self.TLabel3 = ttk.Label(top)
+        self.TLabel3.place(relx=0.807, rely=0.065, height=19, width=35)
+        self.TLabel3.configure(background="#d9d9d9")
+        self.TLabel3.configure(foreground="#000000")
+        self.TLabel3.configure(font="TkDefaultFont")
+        self.TLabel3.configure(relief="flat")
+        self.TLabel3.configure(text='''Grade''')
+
+        self.Scrolledtext1 = ScrolledText(top)
+        self.Scrolledtext1.place(relx=0.749, rely=0.584, relheight=0.326
+                , relwidth=0.235)
+        self.Scrolledtext1.configure(background="white")
+        self.Scrolledtext1.configure(font="TkTextFont")
+        self.Scrolledtext1.configure(foreground="black")
+        self.Scrolledtext1.configure(highlightbackground="#d9d9d9")
+        self.Scrolledtext1.configure(highlightcolor="black")
+        self.Scrolledtext1.configure(insertbackground="black")
+        self.Scrolledtext1.configure(insertborderwidth="3")
+        self.Scrolledtext1.configure(selectbackground="#c4c4c4")
+        self.Scrolledtext1.configure(selectforeground="black")
+        self.Scrolledtext1.configure(width=10)
+        self.Scrolledtext1.configure(wrap="none")
+
+        self.Button1 = tk.Button(top)
+        self.Button1.place(relx=0.327, rely=0.292, height=24, width=33)
+        self.Button1.configure(activebackground="#ececec")
+        self.Button1.configure(activeforeground="#000000")
+        self.Button1.configure(background="#d9d9d9")
+        self.Button1.configure(disabledforeground="#a3a3a3")
+        self.Button1.configure(foreground="#000000")
+        self.Button1.configure(highlightbackground="#d9d9d9")
+        self.Button1.configure(highlightcolor="black")
+        self.Button1.configure(pady="0")
+        self.Button1.configure(text='''Add''')
+
+        self.Button2 = tk.Button(top)
+        self.Button2.place(relx=0.374, rely=0.519, height=24, width=54)
+        self.Button2.configure(activebackground="#ececec")
+        self.Button2.configure(activeforeground="#000000")
+        self.Button2.configure(background="#d9d9d9")
+        self.Button2.configure(disabledforeground="#a3a3a3")
+        self.Button2.configure(foreground="#000000")
+        self.Button2.configure(highlightbackground="#d9d9d9")
+        self.Button2.configure(highlightcolor="black")
+        self.Button2.configure(pady="0")
+        self.Button2.configure(text='''Remove''')
+
+# The following code is added to facilitate the Scrolled widgets you specified.
+class AutoScroll(object):
+    '''Configure the scrollbars for a widget.'''
+
+    def __init__(self, master):
+        #  Rozen. Added the try-except clauses so that this class
+        #  could be used for scrolled entry widget for which vertical
+        #  scrolling is not supported. 5/7/14.
+        try:
+            vsb = ttk.Scrollbar(master, orient='vertical', command=self.yview)
+        except:
+            pass
+        hsb = ttk.Scrollbar(master, orient='horizontal', command=self.xview)
+
+        #self.configure(yscrollcommand=_autoscroll(vsb),
+        #    xscrollcommand=_autoscroll(hsb))
+        try:
+            self.configure(yscrollcommand=self._autoscroll(vsb))
+        except:
+            pass
+        self.configure(xscrollcommand=self._autoscroll(hsb))
+
+        self.grid(column=0, row=0, sticky='nsew')
+        try:
+            vsb.grid(column=1, row=0, sticky='ns')
+        except:
+            pass
+        hsb.grid(column=0, row=1, sticky='ew')
+
+        master.grid_columnconfigure(0, weight=1)
+        master.grid_rowconfigure(0, weight=1)
+
+        # Copy geometry methods of master  (taken from ScrolledText.py)
+        if py3:
+            methods = tk.Pack.__dict__.keys() | tk.Grid.__dict__.keys() \
+                  | tk.Place.__dict__.keys()
+        else:
+            methods = tk.Pack.__dict__.keys() + tk.Grid.__dict__.keys() \
+                  + tk.Place.__dict__.keys()
+
+        for meth in methods:
+            if meth[0] != '_' and meth not in ('config', 'configure'):
+                setattr(self, meth, getattr(master, meth))
+
+    @staticmethod
+    def _autoscroll(sbar):
+        '''Hide and show scrollbar as needed.'''
+        def wrapped(first, last):
+            first, last = float(first), float(last)
+            if first <= 0 and last >= 1:
+                sbar.grid_remove()
+            else:
+                sbar.grid()
+            sbar.set(first, last)
+        return wrapped
+
+    def __str__(self):
+        return str(self.master)
+
+def _create_container(func):
+    '''Creates a ttk Frame with a given master, and use this new frame to
+    place the scrollbars and the widget.'''
+    def wrapped(cls, master, **kw):
+        container = ttk.Frame(master)
+        container.bind('<Enter>', lambda e: _bound_to_mousewheel(e, container))
+        container.bind('<Leave>', lambda e: _unbound_to_mousewheel(e, container))
+        return func(cls, container, **kw)
+    return wrapped
+
+class ScrolledText(AutoScroll, tk.Text):
+    '''A standard Tkinter Text widget with scrollbars that will
+    automatically show/hide as needed.'''
+    @_create_container
+    def __init__(self, master, **kw):
+        tk.Text.__init__(self, master, **kw)
+        AutoScroll.__init__(self, master)
+
+class ScrolledListBox(AutoScroll, tk.Listbox):
+    '''A standard Tkinter Text widget with scrollbars that will
+    automatically show/hide as needed.'''
+    @_create_container
+    def __init__(self, master, **kw):
+        tk.Listbox.__init__(self, master, **kw)
+        AutoScroll.__init__(self, master)
+
+import platform
+def _bound_to_mousewheel(event, widget):
+    child = widget.winfo_children()[0]
+    if platform.system() == 'Windows' or platform.system() == 'Darwin':
+        child.bind_all('<MouseWheel>', lambda e: _on_mousewheel(e, child))
+        child.bind_all('<Shift-MouseWheel>', lambda e: _on_shiftmouse(e, child))
+    else:
+        child.bind_all('<Button-4>', lambda e: _on_mousewheel(e, child))
+        child.bind_all('<Button-5>', lambda e: _on_mousewheel(e, child))
+        child.bind_all('<Shift-Button-4>', lambda e: _on_shiftmouse(e, child))
+        child.bind_all('<Shift-Button-5>', lambda e: _on_shiftmouse(e, child))
+
+def _unbound_to_mousewheel(event, widget):
+    if platform.system() == 'Windows' or platform.system() == 'Darwin':
+        widget.unbind_all('<MouseWheel>')
+        widget.unbind_all('<Shift-MouseWheel>')
+    else:
+        widget.unbind_all('<Button-4>')
+        widget.unbind_all('<Button-5>')
+        widget.unbind_all('<Shift-Button-4>')
+        widget.unbind_all('<Shift-Button-5>')
+
+def _on_mousewheel(event, widget):
+    if platform.system() == 'Windows':
+        widget.yview_scroll(-1*int(event.delta/120),'units')
+    elif platform.system() == 'Darwin':
+        widget.yview_scroll(-1*int(event.delta),'units')
+    else:
+        if event.num == 4:
+            widget.yview_scroll(-1, 'units')
+        elif event.num == 5:
+            widget.yview_scroll(1, 'units')
+
+def _on_shiftmouse(event, widget):
+    if platform.system() == 'Windows':
+        widget.xview_scroll(-1*int(event.delta/120), 'units')
+    elif platform.system() == 'Darwin':
+        widget.xview_scroll(-1*int(event.delta), 'units')
+    else:
+        if event.num == 4:
+            widget.xview_scroll(-1, 'units')
+        elif event.num == 5:
+            widget.xview_scroll(1, 'units')
+
+if __name__ == '__main__':
+    vp_start_gui()
+
+
+
